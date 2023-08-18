a4d70e478c7e663b522b7e2916066eb60dc8ef40
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-18 12:08:19 +0100

    change text lookup to irds

--- src/modelling/get_all_bm25.py
+++ src/modelling/get_all_bm25.py
@@ -7,8 +7,8 @@ from fire import Fire
 import multiprocessing as mp
 import ir_datasets as irds
 
-def main(index_dataset : str, eval_set : str, output_file : str, budget : int = 1000):
-    index = pt.get_dataset(index_dataset).get_index()
+def main(index_dataset : str, eval_set : str, text_lookup : str, output_file : str, budget : int = 1000):
+    index = pt.get_dataset(text_lookup)
     eval_set = irds.load(eval_set)
     num_cpus = mp.cpu_count()
     bm25 = ptp.PisaIndex.from_dataset(index_dataset, threads=num_cpus).bm25(num_results=budget) >> pt.get_text(index, 'text')
