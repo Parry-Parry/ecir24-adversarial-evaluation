e95a04bb315475457f88acd873e4b2591643204a
Maik Fröbe <maik.froebe@uni-jena.de>
2023-09-06 06:02:38 +0200

    mf

--- src/rewriting/rewrite-with-chatnoir-chat.py
+++ src/rewriting/rewrite-with-chatnoir-chat.py
@@ -9,6 +9,14 @@ chat_client = ChatNoirChatClient()
 
 def process_prompt(iteration, prompt):
     prompt_str = json.load(open('prompts.json', 'r'))[prompt]
+    output_file = f'../../data/llm-rewrite/bm25_19_sample_1000_alpacca_prompt_{prompt}_iter_{iteration}.tsv.gz'
+
+    try:
+        pd.read_csv(output_file, names=['qid', 'query', 'docid', 'score', 'rank', 'text'], sep='\t')
+        print(f'Skip prompt {prompt} for iteration {iteration}.')
+    except:
+        pass
+
     df = pd.read_csv('../../data/llm-rewrite/bm25_19_sample_1000.tsv.gz', names=['qid', 'query', 'docid', 'score', 'rank', 'text'], sep='\t')
     ret = []
     for _, i in tqdm(list(df.iterrows())):
@@ -17,10 +25,10 @@ def process_prompt(iteration, prompt):
         ret += [i]
 
     ret = pd.DataFrame(ret)
-    ret.to_csv(f'../../data/llm-rewrite/bm25_19_sample_1000_alpacca_prompt_{prompt}_iter_{iteration}.tsv.gz', sep='\t', header=False, index=False)
+    ret.to_csv(output_file, sep='\t', header=False, index=False)
 
 
 for iteration in ['1', '2', '3', '4', '5']:
-    for prompt in ['1', '2', '3']:
+    for prompt in ['1', '2', '3', '4', '5']:
         process_prompt(iteration, prompt)
 
