80097e1242f4447ccb49a05d2cae5ec5997f69ff
Maik Fröbe <maik.froebe@uni-jena.de>
2023-09-14 15:29:01 +0200

    Add chatGPT variants

--- src/rewriting/rewrite-with-chatgpt.py
+++ src/rewriting/rewrite-with-chatgpt.py
@@ -16,8 +16,24 @@ def process_prompt(iteration, prompt):
     ret = pd.DataFrame(ret)
     ret.to_csv(f'../../data/llm-rewrite/bm25_19_sample_1000_chatgpt_prompt_{prompt}_iter_{iteration}.tsv.gz', sep='\t', header=False, index=False)
 
+def process_prompt_prepend_text(iteration, prompt):
+    prompts = json.load(open(f'chatgpt/text-rewrites-from-chatgpt-raw-prompt-{prompt}.json', 'r'))
+
+    df = pd.read_csv('../../data/llm-rewrite/bm25_19_sample_1000.tsv.gz', names=['qid', 'query', 'docid', 'score', 'rank', 'text'], sep='\t')
+    ret = []
+    for _, i in tqdm(list(df.iterrows())):
+        i = i.to_dict()
+        i['text'] = prompts[i['text']]['gpt-3.5-turbo-response']['choices'][0]['message']['content'] + ' ' + i['text']
+        ret += [i]
+
+    ret = pd.DataFrame(ret)
+    ret.to_csv(f'../../data/llm-rewrite/bm25_19_sample_1000_chatgpt_prompt_{prompt}_iter_{iteration}.tsv.gz', sep='\t', header=False, index=False)
+
 
 for iteration in ['1']:
-    for prompt in ['1', '2']: #, '3', '4', '5']:
+    for prompt in ['1', '2', '3', '4', '5']:
         process_prompt(iteration, prompt)
 
+for iteration in ['1']:
+    for prompt in ['6', '7', '8', '9', '10']:
+        process_prompt_prepend_text(iteration, prompt)
