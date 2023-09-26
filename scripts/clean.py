f6c070ee5f001e08275334ba643990c8b7741707
Andrew Parry <2393265p@student.gla.ac.uk>
2023-09-26 17:38:36 +0100

    small fix

--- scripts/clean.py
+++ scripts/clean.py
@@ -5,11 +5,15 @@ from tqdm import tqdm
 
 def main(path : str): 
     SUBSET = ['qid', 'docno', 'score', 'augmented_score']
-    files = [f for f in os.listdir(path)]
+    files = [f for f in os.listdir(path) if 'normal' not in f]
     for file in tqdm(files):
         df = pd.read_csv(os.path.join(path, file), sep='\t', index_col=False)
-        df = df[SUBSET].copy()
-        df.to_csv(os.path.join(path, file), sep='\t', index=False, header=True)
+        try:
+            df = df[SUBSET].copy()
+            df.to_csv(os.path.join(path, file), sep='\t', index=False, header=True)
+        except KeyError:
+            print(f'File {file} is formatted incorrectly.')
+            continue
 
 if __name__ == '__main__':
     Fire(main)
