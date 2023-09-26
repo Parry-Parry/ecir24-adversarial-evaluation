38eb83b01eaf9746f5b24851ac2ddfd01fd81ccb
Andrew Parry <2393265p@student.gla.ac.uk>
2023-09-26 14:10:52 +0100

    more sanity check

--- src/rewriting/evaluation/join_all.py
+++ src/rewriting/evaluation/join_all.py
@@ -11,18 +11,18 @@ def build_lookup(df, score_col='score', rank_col='rank'):
         frame[qid] = {row.docno : (getattr(row, rank_col), getattr(row, score_col)) for row in sub.itertuples()}
     return frame
 
-def main(run_dir : str, normal_dir : str, out_dir : str):
+def main(run_dir : str, normal_dir : str, out_dir : str, old : bool = False):
     electra = build_lookup(pd.read_csv(join(normal_dir, 'normal_electra.tsv'), sep='\t', index_col=False))
     t5 = build_lookup(pd.read_csv(join(normal_dir, 'normal_t5.tsv'), sep='\t', index_col=False))
-    try:
+    if not old:
         tasb = build_lookup(pd.read_csv(join(normal_dir, 'normal_tasb.tsv'), sep='\t', index_col=False))
         colbert = build_lookup(pd.read_csv(join(normal_dir, 'normal_colbert.tsv'), sep='\t', index_col=False))
         bm25 = build_lookup(pd.read_csv(join(normal_dir, 'normal_bm25.tsv'), sep='\t', index_col=False))
-    except FileNotFoundError:
+    else: 
         tasb = {}
         colbert = {}
         bm25 = {}
-
+    
     files = [f for f in os.listdir(run_dir) if f.endswith('.tsv') and not f.startswith('normal')]
 
     for file in files:
