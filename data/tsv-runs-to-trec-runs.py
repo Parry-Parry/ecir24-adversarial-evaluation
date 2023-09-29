65018e813815dbc480230f44f43c731425ac90f3
Maik Fröbe <maik.froebe@uni-jena.de>
2023-09-29 09:49:45 +0200

    mf

--- data/tsv-runs-to-trec-runs.py
+++ data/tsv-runs-to-trec-runs.py
@@ -8,21 +8,32 @@ def read_run(path):
     return pd.read_csv(path, sep='\t')
 
 def persist_run(run, model, path, score):
-    run['q0'] = '0'
     run['system'] = model
     run['score'] = run[score]
-    run[['qid', 'q0', 'docno', 'rank', score, 'system']].to_csv(path, sep=' ', index=False, header=False)
+    
+    #normalize runs, code from trectools
+    run = run.sort_values(["qid", "score", "docno"], ascending=[True, False, False]).reset_index()
+    run['q0'] = 0
 
-def tsv_runs_to_trec_runs(model):
-    runs = glob(f'data/runs/*{model}.tsv.gz')
+    run = run.groupby("qid")[["qid", "q0", "docno", "score", "system"]].head(100)
+
+    # Make sure that rank position starts by 1
+    run["rank"] = 1
+    run["rank"] = run.groupby("qid")["rank"].cumsum()
+    
+    run[['qid', 'q0', 'docno', 'rank', 'score', 'system']].to_csv(path, sep=' ', index=False, header=False)
+
+def tsv_runs_to_trec_runs(track, model):
+    runs = glob(f'data/runs/{track}/*{model}.tsv.gz')
 
     # format baseline
-    persist_run(read_run(runs[0]), model, f'data/trec-runs/baseline_{model}.trec', 'score')
+    persist_run(read_run(runs[0]), model, f'data/trec-runs/{track}/baseline_{model}.trec.gz', 'score')
 
     for i in tqdm(runs):
         run_name = i.split('/')[-1].split(f'.tsv.gz')[0]
-        persist_run(read_run(i), model, f'data/trec-runs/{run_name}.trec', 'augmented_score')
+        persist_run(read_run(i), model, f'data/trec-runs/{track}/{run_name}.trec.gz', 'augmented_score')
 
-for model in ['t5', 'electra']:
-    tsv_runs_to_trec_runs(model)
+for track in ['dl19', 'dl20']:
+    for model in ['bm25', 'colbert', 't5', 'electra', 'tasb']:
+        tsv_runs_to_trec_runs(track, model)
 
