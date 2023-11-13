6e8672288be481f1bf0e9b29adf7c76badbf0643
Maik Fröbe <maik.froebe@uni-jena.de>
2023-11-13 06:08:13 +0100

    mf

--- token-search/foo.sh
+++ token-search/foo.sh
@@ -1,1 +1,10 @@
-tira-run     --input-directory ${PWD}/t5-base-re-ranking     --image registry.webis.de/code-research/tira/tira-user-tira-ir-starter/pygaggle:0.0.1-monot5-base-msmarco-10k     --command '/reranking.py --input $inputDataset --output $outputDir'
+#!/usr/bin/bash
+
+docker run \
+	--rm -ti \
+	-v ${PWD}:/app \
+	-w /app \
+	--entrypoint /app/score-t5.py \
+	registry.webis.de/code-research/tira/tira-user-tira-ir-starter/pygaggle:0.0.1-monot5-base-msmarco-10k \
+	--input /app/t5-base-re-ranking --output /app/t5-base-re-ranking
+
--- token-search/score-t5.py
+++ token-search/score-t5.py
@@ -1,12 +1,60 @@
 #!/usr/bin/env python3
-import pyterrier as pt
+import os
+import argparse
 import pandas as pd
-pt.start()
-from pyterrier_t5 import MonoT5ReRanker
-monoT5 = MonoT5ReRanker()
+from tqdm import tqdm
+from pygaggle.rerank.base import Query, Text
+import importlib
+from pygaggle.rerank.transformer import MonoT5, MonoBERT
 
-df = pd.read_json('t5-base-re-ranking.json.gz', lines=True)
-#monoT5 = MonoT5ReRanker()
-df = df.head(100)
 
-df = pt.Transformer.from_df(df)
+def parse_args():
+    parser = argparse.ArgumentParser(prog='Re-rank with pygaggle.')
+
+    parser.add_argument('--model_name', default=os.environ['MODEL_NAME'])
+    parser.add_argument('--tokenizer_name', default=os.environ['TOKENIZER_NAME'])
+    parser.add_argument('--input', help='The directory with the input data (i.e., a queries.jsonl and a documents.jsonl file).', required=True)
+    parser.add_argument('--output', type=str, help='The output will be stored in this directory.', required=True)
+
+    return parser.parse_args()
+
+
+def rerank(qid, query, df_docs, model):
+    print(f'Rerank for query "{query}" (qid={qid}).')
+
+    texts = [Text(i['text'], {'docid': i['docno']}, 0) for _, i in df_docs.iterrows()]
+    
+    scores = model.rerank(Query(query), texts)
+    scores = {i.metadata["docid"]: i.score for i in scores}
+    ret = []
+
+    for _, i in df_docs.iterrows():
+        ret += [{'qid': qid, 'query': query, 'docno': i['docno'], 'text': i['text'], 'score': scores[i['docno']]}]
+
+    return ret
+
+
+def main(model_name, tokenizer_name, input_file, output_directory):
+    df = pd.read_json(input_file + '/rerank.jsonl.gz', lines=True)
+    qids = sorted(list(df['qid'].unique()))
+    df_ret = []
+    model = None
+    
+    if 'monot5' in model_name.lower():
+        model = MonoT5(model=MonoT5.get_model(model_name, local_files_only=True), tokenizer=MonoT5.get_tokenizer(tokenizer_name, local_files_only=True))
+    elif 'monobert' in model_name.lower():
+        model = MonoBERT(model=MonoBERT.get_model(model_name, local_files_only=True), tokenizer=MonoBERT.get_tokenizer(tokenizer_name, local_files_only=True))
+
+    for qid in tqdm(qids):
+        df_qid = df[df['qid'] == qid]
+        query = df_qid.iloc[0].to_dict()['query']
+
+        df_ret += rerank(qid, query, df_qid[['docno', 'text']], model)
+
+    pd.DataFrame(df_ret).to_json(output_directory + '/rerank-with-scores.jsonl.gz', lines=True, orient='records')
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args.model_name, args.tokenizer_name, args.input, args.output)
+
