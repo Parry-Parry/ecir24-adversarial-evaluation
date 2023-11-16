d0488089ccd6e3637a7ee3a2b2b6dc764ba4945f
Andrew Parry <2393265p@student.gla.ac.uk>
2023-11-16 10:17:18 +0000

    small change to tas-b flexibility

--- token-search/score-bert-dot.py
+++ token-search/score-bert-dot.py
@@ -13,7 +13,10 @@ def main(input_file : str, output_directory : str, batch_size : int = 64, model_
     model = TasB(model_id, batch_size=batch_size) if 'tas' in model_id else HgfBiEncoder(AutoModel.from_pretrained(model_id), AutoTokenizer.from_pretrained(model_id), batch_size=batch_size)
     for qid in tqdm(qids):
         df_qid = df[df['qid'] == qid]
-        df_qid['doc_vec'] = model.transform(df_qid['text'])
+        if 'tas' in model_id:
+            pass 
+        else:
+            df_qid['doc_vec'] = model.transform(df_qid['text'])
         df_ret.append(model.transform(df_qid))
     pd.DataFrame(df_ret).to_json(output_directory + '/rerank-with-scores.jsonl.gz', lines=True, orient='records')
 
