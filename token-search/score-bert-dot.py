311a7d1d282384a7236c54365a71ad276f3e2c03
Andrew Parry <2393265p@student.gla.ac.uk>
2023-11-16 10:19:02 +0000

    goodness

--- token-search/score-bert-dot.py
+++ token-search/score-bert-dot.py
@@ -7,7 +7,7 @@ from transformers import AutoTokenizer, AutoModel
 
 
 def main(input_file : str, output_directory : str, batch_size : int = 64, model_id : str = 'sebastian-hofstaetter/distilbert-dot-tas_b-b256-msmarco'):
-    df = pd.read_json(input_file + '/rerank.jsonl.gz', lines=True)
+    df = pd.read_json(input_file, lines=True)
     qids = sorted(list(df['qid'].unique()))
     df_ret = []
     model = TasB(model_id, batch_size=batch_size) if 'tas' in model_id else HgfBiEncoder(AutoModel.from_pretrained(model_id), AutoTokenizer.from_pretrained(model_id), batch_size=batch_size)
