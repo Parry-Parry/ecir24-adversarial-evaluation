ae8d0f942d23d147dbb3afa3ca362bce15eb3b74
Andrew Parry <2393265p@student.gla.ac.uk>
2023-11-16 19:00:24 +0000

    simplify the process

--- token-search/score-t5-terrier.py
+++ token-search/score-t5-terrier.py
@@ -7,15 +7,11 @@ from fire import Fire
 
 def score_t5(input_file, output_directory):
     df = pd.read_json(input_file + '/rerank.jsonl.gz', lines=True)
-    qids = sorted(list(df['qid'].unique()))
-    df_ret = []
 
     model = MonoT5ReRanker()
-    for qid in tqdm(qids):
-        df_qid = df[df['qid'] == qid]
-        df_ret.append(model.transform(df_qid))
+    rez = model.transform(df)
 
-    pd.concat(df_ret).to_json(output_directory + '/rerank-with-scores.jsonl.gz', lines=True, orient='records')
+    rez.to_json(output_directory + '/rerank-with-scores.jsonl.gz', lines=True, orient='records')
 
 if __name__ == '__main__':
     Fire(score_t5)
