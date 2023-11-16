edc0203efc32e6463ed1cb0336a91a0baac4cb05
Andrew Parry <2393265p@student.gla.ac.uk>
2023-11-16 18:58:47 +0000

    remove broken imports

--- token-search/score-t5-terrier.py
+++ token-search/score-t5-terrier.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env python3
-import os
-import argparse
 import pandas as pd
 from tqdm import tqdm
-import importlib
 from pyterrier_t5 import MonoT5ReRanker
+from fire import Fire
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(prog='Re-rank with pygaggle.')
-
-    parser.add_argument('--model_name', default=os.environ['MODEL_NAME'])
-    parser.add_argument('--tokenizer_name', default=os.environ['TOKENIZER_NAME'])
-    parser.add_argument('--input', help='The directory with the input data (i.e., a queries.jsonl and a documents.jsonl file).', required=True)
-    parser.add_argument('--output', type=str, help='The output will be stored in this directory.', required=True)
-
-    return parser.parse_args()
-
-
-def main(input_file, output_directory):
+def score_t5(input_file, output_directory):
     df = pd.read_json(input_file + '/rerank.jsonl.gz', lines=True)
     qids = sorted(list(df['qid'].unique()))
     df_ret = []
@@ -30,8 +17,6 @@ def main(input_file, output_directory):
 
     pd.concat(df_ret).to_json(output_directory + '/rerank-with-scores.jsonl.gz', lines=True, orient='records')
 
-
 if __name__ == '__main__':
-    args = parse_args()
-    main(args.model_name, args.tokenizer_name, args.input, args.output)
+    Fire(score_t5)
 
