4560ec9c37105a01742026416614a44a711f2f9b
Andrew Parry <2393265p@student.gla.ac.uk>
2023-11-16 19:04:21 +0000

    16 x batch

--- token-search/score-t5-terrier.py
+++ token-search/score-t5-terrier.py
@@ -8,10 +8,10 @@ from pyterrier_t5 import MonoT5ReRanker
 from fire import Fire
 
 
-def score_t5(input_file, output_directory):
+def score_t5(input_file, output_directory, batch_size : int = 64):
     df = pd.read_json(input_file + '/rerank.jsonl.gz', lines=True)
 
-    model = MonoT5ReRanker()
+    model = MonoT5ReRanker(batch_size=batch_size)
     rez = model.transform(df)
 
     rez.to_json(output_directory + '/rerank-with-scores.jsonl.gz', lines=True, orient='records')
