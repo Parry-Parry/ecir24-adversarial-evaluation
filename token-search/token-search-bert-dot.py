4cd21ab30cc5e16eafd51f8af419eb1c6a8ac3ec
Andrew Parry <2393265p@student.gla.ac.uk>
2023-11-16 11:48:40 +0000

    adapting code

--- token-search/token-search-bert-dot.py
+++ token-search/token-search-bert-dot.py
@@ -4,27 +4,32 @@ from transformers import AutoTokenizer
 from tqdm import tqdm
 import random
 from fire import Fire
-from os.path import join
 
-def main(out_dir : str, model_id : str = 'sebastian-hofstaetter/distilbert-dot-tas_b-b256-msmarco'):
+
+def sample_bert(out_file : str, model_id : str = 'sebastian-hofstaetter/distilbert-dot-tas_b-b256-msmarco', query_file : str = None):
+
     tokens = set()
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    for term in tokenizer.get_vocab().keys():
-        if(term[0].isalnum()):
+
+    for term in AutoTokenizer.from_pretrained(model_id).get_vocab().keys():
+        if len(term) > 1 and not term[0].isalnum() and term[1].isalnum():
             tokens.add(term)
 
     tokens = list(tokens)
     ret = []
-    queries = [i for i in tokens]
-    random.shuffle(queries)
-    queries = queries[:50]
+    if query_file:
+        queries = pd.read_json(query_file, lines=True)
+        queries = queries['query'].tolist()
+    else:
+        queries = [i for i in tokens]
+        random.shuffle(queries)
+        queries = queries[:50]
 
     for qid, query in tqdm(zip(range(len(queries)), queries)):
         for docid, doc in zip(range(len(tokens)), tokens):
             ret += [{"qid": qid, "query": query, "docno": docid, "text": doc, "rank": docid, "score": docid, "original_document": {}, "original_query": {}}]
 
-    pd.DataFrame(ret).to_json(join(out_dir, 'bert-dot-re-ranking.json.gz'), lines=True, orient='records')
+    pd.DataFrame(ret).to_json(out_file, lines=True, orient='records')
 
 if __name__ == '__main__':
-    Fire(main)
+    Fire(sample_bert)
 
--- token-search/token-search-t5.py
+++ token-search/token-search-t5.py
@@ -4,21 +4,27 @@ from transformers import T5Tokenizer
 from tqdm import tqdm
 import random
 
-tokens = set()
+def sample_t5(out_file : str, query_file : str = None):
 
-for term in T5Tokenizer.from_pretrained("t5-base").get_vocab().keys():
-    if len(term) > 1 and not term[0].isalnum() and term[1].isalnum():
-        tokens.add(term)
+    tokens = set()
 
-tokens = list(tokens)
-ret = []
-queries = [i for i in tokens]
-random.shuffle(queries)
-queries = queries[:50]
+    for term in T5Tokenizer.from_pretrained("t5-base").get_vocab().keys():
+        if len(term) > 1 and not term[0].isalnum() and term[1].isalnum():
+            tokens.add(term)
 
-for qid, query in tqdm(zip(range(len(queries)), queries)):
-    for docid, doc in zip(range(len(tokens)), tokens):
-        ret += [{"qid": qid, "query": query, "docno": docid, "text": doc, "rank": docid, "score": docid, "original_document": {}, "original_query": {}}]
+    tokens = list(tokens)
+    ret = []
+    if query_file:
+        queries = pd.read_json(query_file, lines=True)
+        queries = queries['query'].tolist()
+    else:
+        queries = [i for i in tokens]
+        random.shuffle(queries)
+        queries = queries[:50]
 
-pd.DataFrame(ret).to_json('t5-base-re-ranking.json.gz', lines=True, orient='records')
+    for qid, query in tqdm(zip(range(len(queries)), queries)):
+        for docid, doc in zip(range(len(tokens)), tokens):
+            ret += [{"qid": qid, "query": query, "docno": docid, "text": doc, "rank": docid, "score": docid, "original_document": {}, "original_query": {}}]
+
+    pd.DataFrame(ret).to_json(out_file, lines=True, orient='records')
 
