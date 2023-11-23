372c47f3bf8ba1efef4c84059ddd0c04fdbf9dfc
Andrew Parry <2393265p@student.gla.ac.uk>
2023-11-23 10:19:56 +0000

    eesh

--- token-search/score-bert-dot.py
+++ token-search/score-bert-dot.py
@@ -11,18 +11,11 @@ from transformers import AutoTokenizer, AutoModel
 
 def score_bert(in_file : str, out_dir : str, batch_size : int = 64, model_id : str = 'sebastian-hofstaetter/distilbert-dot-tas_b-b256-msmarco'):
     df = pd.read_json(in_file, lines=True)
-    qids = sorted(list(df['qid'].unique()))
-    df_ret = []
     model = TasB(model_id, batch_size=batch_size) if 'tas' in model_id else HgfBiEncoder(AutoModel.from_pretrained(model_id), AutoTokenizer.from_pretrained(model_id), batch_size=batch_size)
     model = BiScorer(model)
-    for qid in tqdm(qids):
-        df_qid = df[df['qid'] == qid]
-        if 'tas' in model_id:
-            pass 
-        else:
-            df_qid['doc_vec'] = model.transform(df_qid['text'])
-        df_ret.append(model.transform(df_qid))
-    pd.concat(df_ret).to_json(out_dir + '/rerank-with-scores.jsonl.gz', lines=True, orient='records')
+    df_ret = model.transform(df)
+    
+    df_ret.to_json(out_dir + '/rerank-with-scores.jsonl.gz', lines=True, orient='records')
 
 if __name__ == '__main__':
     Fire(score_bert)
