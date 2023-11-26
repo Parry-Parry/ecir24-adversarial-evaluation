845068ca160899f79ed345d1e95cec8630b3b4a0
Andrew Parry <2393265p@student.gla.ac.uk>
2023-11-26 13:40:01 +0000

    refactor

--- token-search/token-search.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-docker run \
-	--rm -ti \
-	-w /app \
-	-v ${PWD}:/app \
-	--entrypoint ./token-search.sh \
-	registry.webis.de/code-research/tira/tira-user-tira-ir-starter/pygaggle:0.0.1-monot5-base-msmarco-10k
-
